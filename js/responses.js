019fd8d7c166d124ce28f1ad4ed3b53175368914
Yegor Bugayenko <yegor256@gmail.com>
2019-05-01 08:08:42 +0300

    #14 markers

--- js/responses.js
+++ js/responses.js
@@ -8,10 +8,25 @@ function on_schedule(label, f) {
   });
 }
 
+function on_mnemo(id) {
+  "use strict";
+  var $span = $("#" + id);
+  $span.on("click", function() {
+    $("#schedule").val($span.text());
+    return false;
+  });
+}
+
 $(function() {
   "use strict";
   on_schedule("asap", function(today) { return dateFns.addDays(today, 3); });
   on_schedule("week", function(today) { return dateFns.addWeeks(today, 1); });
   on_schedule("month", function(today) { return dateFns.addMonths(today, 1); });
   on_schedule("later", function(today) { return dateFns.addMonths(today, 3); });
+  on_mnemo("daily");
+  on_mnemo("weekly");
+  on_mnemo("biweekly");
+  on_mnemo("monthly");
+  on_mnemo("quarterly");
+  on_mnemo("annually");
 });
