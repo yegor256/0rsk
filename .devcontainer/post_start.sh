bbf6f4771a4d1625febd57972c4cb1b1cfafdad2
Artur <artur@intech.ee>
2025-06-15 17:41:35 +0000

    127.0.0.1 instead of localhost

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -3,4 +3,4 @@
 # https://github.com/yegor256/0rsk/issues/152
 nohup bash -c 'ruby 0rsk.rb -p $SINATRA_PORT -e development &' >$WORKSPACE_DIR/sinatra.log 2>&1
 
-echo "Sinatra should have been started at http://localhost:$SINATRA_PORT"
+echo "Sinatra should have been started at http://127.0.0.1:$SINATRA_PORT"
