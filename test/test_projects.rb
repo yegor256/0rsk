57c3c2c70c81b95e47ab49cf2b49f8db0efee310
Ghost <ghxstofsolana@gmail.com>
2026-05-29 02:58:45 +0200

    test(#205): assert deleted triple is gone

--- test/test_projects.rb
+++ test/test_projects.rb
@@ -7,6 +7,10 @@ require 'yaml'
 require_relative 'test__helper'
 require_relative '../objects/rsk'
 require_relative '../objects/causes'
+require_relative '../objects/effects'
+require_relative '../objects/projects'
+require_relative '../objects/risks'
+require_relative '../objects/triples'
 
 # Test of Projects.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
@@ -24,12 +28,12 @@ class Rsk::ProjectsTest < Minitest::Test
 
   def test_deletes_with_triple
     projects = Rsk::Projects.new(test_pgsql, 'jeff094')
-    project = projects.add("testfs#{rand(99_999)}")
-    cid = Rsk::Causes.new(test_pgsql, project).add('we have data')
-    rid = Rsk::Risks.new(test_pgsql, project).add('we may lose it')
-    eid = Rsk::Effects.new(test_pgsql, project).add('business will stop NOW')
-    triples = Rsk::Triples.new(test_pgsql, project)
-    triples.add(cid, rid, eid)
-    projects.delete(project)
+    pid = projects.add("testfs#{rand(99_999)}")
+    cid = Rsk::Causes.new(test_pgsql, pid).add('we have data')
+    rid = Rsk::Risks.new(test_pgsql, pid).add('we may lose it')
+    eid = Rsk::Effects.new(test_pgsql, pid).add('business will stop NOW')
+    tid = Rsk::Triples.new(test_pgsql, pid).add(cid, rid, eid)
+    projects.delete(pid)
+    assert_empty(test_pgsql.exec('SELECT * FROM triple WHERE id = $1', [tid]))
   end
 end
