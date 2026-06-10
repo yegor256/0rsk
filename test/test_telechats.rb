cada7751289a22dcf5cbebbb2350528a95c65c57
VasilevNStas <ctac@outlook.com>
2026-06-10 10:33:04 +0300

    #316: add telechats tests for exists, login_of, chat_of, wired, double_add

--- test/test_telechats.rb
+++ test/test_telechats.rb
@@ -21,4 +21,33 @@ class Rsk::TelechatsTest < Minitest::Test
     refute(telechats.diff?(msg, chat))
     assert(telechats.diff?('something else', chat))
   end
+
+  def test_adds_and_fetches
+    login = "judyAF#{rand(99_999)}"
+    telechats = Rsk::Telechats.new(test_pgsql)
+    chat = rand(99_999)
+    telechats.add(chat, login)
+    assert(telechats.exists?(chat))
+    assert_equal(login, telechats.login_of(chat))
+    assert_equal(chat, telechats.chat_of(login))
+  end
+
+  def test_wired
+    login = "judyW#{rand(99_999)}"
+    telechats = Rsk::Telechats.new(test_pgsql)
+    chat = rand(99_999)
+    refute(telechats.wired?(login))
+    telechats.add(chat, login)
+    assert(telechats.wired?(login))
+  end
+
+  def test_double_add
+    login = "judyDA#{rand(99_999)}"
+    telechats = Rsk::Telechats.new(test_pgsql)
+    chat = rand(99_999)
+    telechats.add(chat, login)
+    assert_raises(PG::UniqueViolation) do
+      telechats.add(chat, 'other')
+    end
+  end
 end
