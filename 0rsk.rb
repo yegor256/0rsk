1e4eeccb3643e0833750689bd39e15575eba126d
VasilevNStas <ctac@outlook.com>
2026-06-11 15:59:41 +0300

    #192: use POST for /ranked/delete, keep GET for backward compat

--- 0rsk.rb
+++ 0rsk.rb
@@ -100,6 +100,12 @@ get '/ranked/delete' do
   flash('/ranked', "The ranked triple ##{id} deleted")
 end
 
+post '/ranked/delete' do
+  id = params[:id]
+  triples.delete(id)
+  flash('/ranked', "The ranked triple ##{id} deleted")
+end
+
 get '/projects' do
   haml :projects, layout: :layout, locals: merged(
     title: '/projects',
