e97d955130cd202e53569ce566e832a48b0aaf3a
VasilevNStas <ctac@outlook.com>
2026-06-19 12:22:39 +0300

    #182: replace Thin with Puma

--- 0rsk.rb
+++ 0rsk.rb
@@ -36,7 +36,6 @@ configure do
     c.release = Rsk::VERSION
   end
   set :bind, '0.0.0.0'
-  set :server, :thin
   set :show_exceptions, false
   set :raise_errors, false
   set :dump_errors, false
