d69f9516b67b8bdfa3f3ec2124a82e950282d4f3
Vasilev Stas <33578155+VasilevNStas@users.noreply.github.com>
2026-06-26 08:21:20 +0300

    #364 Plans#fetch returns duplicate rows when a part belongs to more than one triple (#368)
    * #364 Plans#fetch deduplication with DISTINCT ON
    
    * #364 rubocop fix
    
    * #364: fix RuboCop offenses in Rakefile
    
    * #364: fix method name for Elegant/GoodMethodName

--- test/test_plans.rb
+++ test/test_plans.rb
@@ -5,10 +5,13 @@
 
 require_relative 'test__helper'
 
+require_relative '../objects/causes'
+require_relative '../objects/effects'
 require_relative '../objects/plans'
 require_relative '../objects/projects'
 require_relative '../objects/risks'
 require_relative '../objects/rsk'
+require_relative '../objects/triples'
 
 class Rsk::PlansTest < Minitest::Test
   def test_adds_and_fetches
@@ -23,5 +26,18 @@ class Rsk::PlansTest < Minitest::Test
     refute_empty(plans.fetch)
     assert(plans.fetch.any? { |p| p[:text] == text })
     plans.get(id, rid).complete
+  end
+
+  def test_fetch_no_duplicates
+    pid = Rsk::Projects.new(test_pgsql, "dups#{rand(99_999)}").add("test#{rand(99_999)}")
+    cid = Rsk::Causes.new(test_pgsql, pid).add('some cause')
+    eid = Rsk::Effects.new(test_pgsql, pid).add('some effect')
+    triples = Rsk::Triples.new(test_pgsql, pid)
+    triples.add(cid, Rsk::Risks.new(test_pgsql, pid).add('risk one'), eid)
+    triples.add(cid, Rsk::Risks.new(test_pgsql, pid).add('risk two'), eid)
+    plans = Rsk::Plans.new(test_pgsql, pid)
+    plans.add(cid, 'mitigate it')
+    assert_equal(1, plans.count)
+    assert_equal(1, plans.fetch.length)
   end
 end
