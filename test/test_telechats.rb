3d6296fb9b3b7926cdbfc3fde0d51b5239bb2e0b
VasilevNStas <ctac@outlook.com>
2026-07-03 19:13:00 +0300

    fix: SecureRandom, login_of->login, ArgumentAlignment

--- test/test_telechats.rb
+++ test/test_telechats.rb
@@ -5,12 +5,13 @@
 
 require_relative 'test__helper'
 
+require 'securerandom'
 require_relative '../objects/telechats'
 
 class Rsk::TelechatsTest < Minitest::Test
   def test_checks
     telechats = Rsk::Telechats.new(test_pgsql)
-    chat = rand(99_999)
+    chat = SecureRandom.random_number(2_000_000_000) + 1
     telechats.add(chat, "judy#{rand(99_999)}")
     msg = 'hey, you!'
     telechats.posted(msg, chat)
@@ -21,27 +22,25 @@ class Rsk::TelechatsTest < Minitest::Test
   def test_adds_and_fetches
     login = "judyAF#{rand(99_999)}"
     telechats = Rsk::Telechats.new(test_pgsql)
-    chat = rand(99_999)
+    chat = SecureRandom.random_number(2_000_000_000) + 1
     telechats.add(chat, login)
     assert(telechats.exists?(chat))
-    assert_equal(login, telechats.login_of(chat))
-    assert_equal(chat, telechats.chat_of(login))
+    assert_equal(login, telechats.login(chat))
+    assert_equal(chat, telechats.chat(login))
   end
 
   def test_wired
     login = "judyW#{rand(99_999)}"
     telechats = Rsk::Telechats.new(test_pgsql)
-    chat = rand(99_999)
     refute(telechats.wired?(login))
-    telechats.add(chat, login)
+    telechats.add(SecureRandom.random_number(2_000_000_000) + 1, login)
     assert(telechats.wired?(login))
   end
 
   def test_double_add
-    login = "judyDA#{rand(99_999)}"
     telechats = Rsk::Telechats.new(test_pgsql)
-    chat = rand(99_999)
-    telechats.add(chat, login)
+    chat = SecureRandom.random_number(2_000_000_000) + 1
+    telechats.add(chat, "judyDA#{rand(99_999)}")
     assert_raises(PG::UniqueViolation) do
       telechats.add(chat, 'other')
     end
