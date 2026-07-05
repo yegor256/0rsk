cc38420a37f3c52e9377d331eb2bff91e3e5bfbd
VasilevNStas <ctac@outlook.com>
2026-07-05 23:30:05 +0300

    #21-f: Add TaskTracker daemon with timeout-protected GitHub pushes, fix dedup key

--- front/front_tasks.rb
+++ front/front_tasks.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'json'
+require 'timeout'
+
 require_relative '../objects/daemon'
 require_relative '../objects/pipeline'
 # SPDX-FileCopyrightText: Copyright (c) 2019-2026 Yegor Bugayenko
@@ -7,10 +10,30 @@ require_relative '../objects/pipeline'
 
 require_relative '../objects/postpone'
 require_relative '../objects/tasks'
+require_relative '../objects/tasktracker'
+require_relative '../objects/trackers'
 
 Rsk::Daemon.new(10).start do
   users.fetch.each do |login|
     tasks(login: login).create
+    Rsk::Projects.new(settings.pgsql, login).fetch.each do |project|
+      Rsk::Trackers.new(settings.pgsql, project[:id]).fetch(token: true).each do |trk|
+        gh = Rsk::TaskTracker.new(trk[:repo], trk[:token])
+        tasks(login: login).fetch(limit: 100).each do |task|
+          next if task[:tracker_data] && JSON.parse(task[:tracker_data])['repo'] == trk[:repo]
+          begin
+            Timeout.timeout(15) do
+              issue = gh.create(task)
+              tasks(login: login).track(task[:id], trk[:repo], issue)
+            end
+          rescue Timeout::Error => e
+            settings.log.error("GitHub push for task ##{task[:id]} timed out: #{e.message}")
+          rescue StandardError => e
+            settings.log.error("GitHub push failed for task ##{task[:id]}: #{e.message}")
+          end
+        end
+      end
+    end
   end
   @updated = Time.now
 end
