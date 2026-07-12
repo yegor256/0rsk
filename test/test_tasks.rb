e217ae9184833150f386435e2a76c72a6bf96e75
ext11n7 <ext11n7@users.noreply.github.com>
2026-07-13 00:25:21 +0300

    fix(#398): simplify duplicate task regression test

--- test/test_tasks.rb
+++ test/test_tasks.rb
@@ -66,9 +66,7 @@ class Rsk::TasksTest < TestCase
     plans.get(pid, eid).reschedule((Time.now - (5 * 24 * 60 * 60)).strftime('%d-%m-%Y'))
     test_pgsql.exec('INSERT INTO task (plan) VALUES ($1)', [pid])
     tasks = Rsk::Tasks.new(test_pgsql, login)
-    pipeline = Object.new
-    pipeline.define_singleton_method(:fetch) { [pid] }
-    Rsk::Pipeline.stub(:new, ->(*_) { pipeline }) { tasks.create }
+    tasks.create
     assert_equal(1, Integer(test_pgsql.exec('SELECT COUNT(*) AS c FROM task WHERE plan = $1', [pid])[0]['c']))
   end
 end
