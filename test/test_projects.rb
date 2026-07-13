2e686ccbef83da32b39834c768f095dbb3b694d3
ext11n7 <ext11n7@users.noreply.github.com>
2026-07-13 17:37:09 +0300

    fix(#205): assert project deletion in test

--- test/test_projects.rb
+++ test/test_projects.rb
@@ -20,12 +20,13 @@ class Rsk::ProjectsTest < TestCase
 
   def test_deletes_with_triple
     projects = Rsk::Projects.new(test_pgsql, 'jeff094')
-    project = projects.add("testfs#{rand(99_999)}")
-    Rsk::Triples.new(test_pgsql, project).add(
-      Rsk::Causes.new(test_pgsql, project).add('we have data'),
-      Rsk::Risks.new(test_pgsql, project).add('we may lose it'),
-      Rsk::Effects.new(test_pgsql, project).add('business will stop NOW')
+    project_id = projects.add("testfs#{rand(99_999)}")
+    Rsk::Triples.new(test_pgsql, project_id).add(
+      Rsk::Causes.new(test_pgsql, project_id).add('we have data'),
+      Rsk::Risks.new(test_pgsql, project_id).add('we may lose it'),
+      Rsk::Effects.new(test_pgsql, project_id).add('business will stop NOW')
     )
-    projects.delete(project)
+    projects.delete(project_id)
+    assert_empty(test_pgsql.exec('SELECT id FROM project WHERE id = $1', [project_id]).to_a)
   end
 end
