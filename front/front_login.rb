2f6f8ffa393c12c81bb300f95e744f4fde759c4a
Vasilev Stas <33578155+VasilevNStas@users.noreply.github.com>
2026-09-01 09:43:29 +0300

    #302: add rate limiting for POST requests (#302)
    * #302: add rate limiting for POST requests
    
    * #302: rubocop fix — modifier if

--- front/front_login.rb
+++ front/front_login.rb
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: MIT
 
 before '/*' do
+  now = Time.now.to_i
+  settings.rate_limits.reject! { |t| t < now - 60 }
+  if request.post?
+    settings.rate_limits << now
+    halt 429, { 'Content-Type' => 'text/plain' }, 'Too many requests' if settings.rate_limits.size > 10
+  end
   @locals = { http_start: Time.now, ver: Rsk::VERSION, login_link: settings.glogin.login_uri, request_ip: request.ip }
   response.set_cookie('glogin', params[:glogin]) if params[:glogin]
   if request.cookies['glogin']
