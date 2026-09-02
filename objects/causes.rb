b77f7f46fb547865007e9979c3282562b1935f92
devil <europedreadlydevil@gmail.com>
2026-09-02 08:48:20 +0000

    #206: say plainly that the cause, risk or effect is already there

--- objects/causes.rb
+++ objects/causes.rb
@@ -6,6 +6,7 @@ require_relative 'query'
 # SPDX-License-Identifier: MIT
 
 require_relative 'rsk'
+require_relative 'urror'
 
 class Rsk::Causes
   def initialize(pgsql, project)
@@ -24,6 +25,8 @@ class Rsk::Causes
       t.exec('INSERT INTO cause (id) VALUES ($1)', [id])
       id
     end
+  rescue PG::UniqueViolation
+    raise(Rsk::Urror, "Cause \"#{text}\" already exists in this project")
   end
 
   def emojis
