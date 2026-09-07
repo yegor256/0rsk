83105f78b00a8a7495a9db0261b4e3c0528778bc
devil <europedreadlydevil@gmail.com>
2026-09-07 20:55:35 +0000

    #491: inline the variables rubocop wants inlined

--- test/test_projects_page.rb
+++ test/test_projects_page.rb
@@ -19,13 +19,11 @@ class Rsk::ProjectsPageTest < TestCase
 
   def test_puts_the_delete_button_inside_its_form
     login = "u#{SecureRandom.hex(8)}"
-    pid = Rsk::Projects.new(test_pgsql, login).add("p#{SecureRandom.hex(8)}")
     set_cookie("glogin=#{login}")
-    set_cookie("0rsk-project=#{pid}")
+    set_cookie("0rsk-project=#{Rsk::Projects.new(test_pgsql, login).add("p#{SecureRandom.hex(8)}")}")
     get('/projects')
     assert_equal(200, last_response.status, last_response.body)
-    html = Nokogiri::HTML.parse(last_response.body)
-    form = html.xpath("//form[contains(@action, '/projects/delete')]").first
+    form = Nokogiri::HTML.parse(last_response.body).xpath("//form[contains(@action,'/projects/delete')]").first
     refute_nil(form, last_response.body)
     refute_empty(form.xpath('.//button'), "the Delete button must be inside its form: #{last_response.body}")
   end
