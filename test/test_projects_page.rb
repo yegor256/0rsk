45ca1594963750f74db42e5820f96b878e6a62cf
devil <europedreadlydevil@gmail.com>
2026-09-07 20:49:10 +0000

    #491: put the Delete button inside the form it submits

---
+++ test/test_projects_page.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2019-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require 'nokogiri'
+require 'rack/test'
+require 'securerandom'
+require_relative 'test__helper'
+
+require_relative '../0rsk'
+
+class Rsk::ProjectsPageTest < TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_puts_the_delete_button_inside_its_form
+    login = "u#{SecureRandom.hex(8)}"
+    pid = Rsk::Projects.new(test_pgsql, login).add("p#{SecureRandom.hex(8)}")
+    set_cookie("glogin=#{login}")
+    set_cookie("0rsk-project=#{pid}")
+    get('/projects')
+    assert_equal(200, last_response.status, last_response.body)
+    html = Nokogiri::HTML.parse(last_response.body)
+    form = html.xpath("//form[contains(@action, '/projects/delete')]").first
+    refute_nil(form, last_response.body)
+    refute_empty(form.xpath('.//button'), "the Delete button must be inside its form: #{last_response.body}")
+  end
+end
