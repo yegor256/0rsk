ff6ae30988851a9e018529f1d319393983120820
devil <europedreadlydevil@gmail.com>
2026-09-07 09:16:42 +0000

    #465: let Haml escape by default

--- 0rsk.rb
+++ 0rsk.rb
@@ -30,7 +30,7 @@ if ENV['RACK_ENV'] != 'test'
 end
 
 configure do
-  set :haml, format: :xhtml, escape_html: false
+  set :haml, format: :xhtml, escape_html: true
   config = { 'github' => { 'client_id' => '?', 'client_secret' => '?', 'encryption_secret' => '' }, 'sentry' => '' }
   cfg = File.join(File.dirname(__FILE__), 'config.yml')
   if File.exist?(cfg)
