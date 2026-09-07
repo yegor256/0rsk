964230dc83f90ce0a5edd3507952f808209b0446
devil <europedreadlydevil@gmail.com>
2026-09-07 20:54:49 +0000

    #532: inline the variables rubocop wants inlined

--- test/test_plan_detach.rb
+++ test/test_plan_detach.rb
@@ -11,10 +11,9 @@ require_relative '../objects/plans'
 class Rsk::PlanDetachTest < TestCase
   def test_refuses_a_part_the_plan_does_not_hang_on
     project = test_project
-    risk = test_risk(project: project)
     cause = test_cause(project: project)
     plans = Rsk::Plans.new(test_pgsql, project)
-    id = plans.add(risk, "plan #{SecureRandom.hex(8)}")
+    id = plans.add(test_risk(project: project), "plan #{SecureRandom.hex(8)}")
     assert_raises(Rsk::Urror) { plans.get(id, cause).detach }
     assert_equal(1, plans.count(query: ''), 'the plan must still be there')
   end
@@ -23,8 +22,7 @@ class Rsk::PlanDetachTest < TestCase
     project = test_project
     risk = test_risk(project: project)
     plans = Rsk::Plans.new(test_pgsql, project)
-    id = plans.add(risk, "plan #{SecureRandom.hex(8)}")
-    plans.get(id, risk).detach
+    plans.get(plans.add(risk, "plan #{SecureRandom.hex(8)}"), risk).detach
     assert_equal(0, plans.count(query: ''), 'the plan must be gone')
   end
 end
