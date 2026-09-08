df80621ec807d7c837b8918f0155878509ad8f8b
morphqdd <europeDreadlyDevil@gmail.com>
2026-09-08 09:10:38 +0300

    #488: hang the id on the link, not on its title (#528)
    * #488: hang the id on the link, not on its title
    
    * #488: test that the three pages render with a plan attached
    
    * #488: keep the requires in the order rubocop wants

---
+++ test/test_responses.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2019-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require 'rack/test'
+require 'securerandom'
+require_relative 'test__helper'
+
+require_relative '../0rsk'
+require_relative '../objects/plans'
+require_relative '../objects/triples'
+
+class Rsk::ResponsesTest < TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_renders_the_responses_page
+    login = "u#{SecureRandom.hex(8)}"
+    project = Rsk::Projects.new(test_pgsql, login).add("p#{SecureRandom.hex(8)}")
+    risk = Rsk::Risks.new(test_pgsql, project).add("risk #{SecureRandom.hex(8)}")
+    triple = Rsk::Triples.new(test_pgsql, project).add(
+      Rsk::Causes.new(test_pgsql, project).add("cause #{SecureRandom.hex(8)}"),
+      risk,
+      Rsk::Effects.new(test_pgsql, project).add("effect #{SecureRandom.hex(8)}")
+    )
+    Rsk::Plans.new(test_pgsql, project).add(risk, "plan #{SecureRandom.hex(8)}")
+    set_cookie("glogin=#{login}")
+    set_cookie("0rsk-project=#{project}")
+    ['/plans', '/tasks', "/responses?id=#{triple}"].each do |uri|
+      get(uri)
+      assert_equal(200, last_response.status, "#{uri} fails: #{last_response.body}")
+    end
+  end
+end
