c0890527d24123a8cfdf8f384f9e6877bbce73a1
devil <europedreadlydevil@gmail.com>
2026-09-23 19:54:48 +0000

    #526: test that a tracker needs a repository and a token

--- test/test_trackers.rb
+++ test/test_trackers.rb
@@ -14,4 +14,20 @@ class Rsk::TrackersTest < TestCase
     trackers.add('gitlab', "foo/#{SecureRandom.hex(8)}", 'secret')
     assert_equal('gitlab', trackers.fetch.first[:type])
   end
+
+  def test_refuses_a_field_the_column_cannot_hold
+    trackers = Rsk::Trackers.new(test_pgsql, test_project)
+    bad = [[nil, 'x'], ['', 'x'], ['  ', 'x'], ['a/b', nil], ['a/b', ''], ['r' * 300, 'x']]
+    bad.each do |repo, token|
+      assert_raises(Rsk::Urror, "#{repo.inspect}/#{token.inspect} must be refused") do
+        trackers.add('github', repo, token)
+      end
+    end
+  end
+
+  def test_adds_a_tracker
+    trackers = Rsk::Trackers.new(test_pgsql, test_project)
+    trackers.add('github', '  foo/bar  ', 'tkn')
+    assert_equal('foo/bar', trackers.fetch[0][:repo], 'the repository must be trimmed')
+  end
 end
